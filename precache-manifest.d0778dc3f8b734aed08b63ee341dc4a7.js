82f53509deb368d54aad8ebdb63259177ff2062f
elvisthermo <elvis.thermo@gmail.com>
2020-06-24 21:13:04 -0300

    Updates

---
+++ precache-manifest.d0778dc3f8b734aed08b63ee341dc4a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c87731e787b6309ef44b791f7a4c46",
+    "url": "/cupu-tech/index.html"
+  },
+  {
+    "revision": "1fd966f09ddb12c6e117",
+    "url": "/cupu-tech/static/css/main.06ec8cfc.chunk.css"
+  },
+  {
+    "revision": "c1028d07d98e1e21083f",
+    "url": "/cupu-tech/static/js/2.6b8bf02c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cupu-tech/static/js/2.6b8bf02c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd966f09ddb12c6e117",
+    "url": "/cupu-tech/static/js/main.53ba0bad.chunk.js"
+  },
+  {
+    "revision": "0717a7e3ef3e6baad177",
+    "url": "/cupu-tech/static/js/runtime-main.c6c4bc81.js"
+  }
+]);
